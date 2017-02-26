2905c306c3cfac00638d428c6092ca6952d8388f
Mike <mhederstedt@me.com>
2017-02-26 09:51:18 +0100

    Update Inlamningsuppgift16.js

--- js/Inlamningsuppgift16.js
+++ js/Inlamningsuppgift16.js
@@ -123,7 +123,7 @@ class App extends React.Component {
         else {
             this.componentWillUnmount();
             let elId = 0;
-            if(typeof(apiData) == undefined)
+            if(typeof apiData === 'undefined')
                 {
                     this.setState({
                         error:"there is a problem"
